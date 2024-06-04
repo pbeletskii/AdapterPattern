8fec42ae94f33df91c9648428ec281975218ff4f
User <beletskiypr@mer.ci.nsu.ru>
2024-06-05 01:27:16 +0700

    Добавьте файлы проекта.

---
+++ ComputerGameAdapter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class ComputerGameAdapter : PCGame
+    {
+        private ComputerGame computerGame;
+
+        public ComputerGameAdapter(ComputerGame computerGame)
+        {
+            this.computerGame = computerGame;
+        }
+
+        public string getTitle()
+        {
+            return computerGame.getName();
+        }
+
+        public int getPegiAllowedAge()
+        {
+            switch (computerGame.getPegiAgeRating())
+            {
+                case PegiAgeRating.P3:
+                    return 3;
+                case PegiAgeRating.P7:
+                    return 7;
+                case PegiAgeRating.P12:
+                    return 12;
+                case PegiAgeRating.P16:
+                    return 16;
+                case PegiAgeRating.P18:
+                    return 18;
+                default:
+                    return 0;
+            }
+        }
+
+        public bool isTripleAGame()
+        {
+            return computerGame.getBudgetInMillionsOfDollars() > 50;
+        }
+
+        public Requirements getRequirements()
+        {
+            return new Requirements(
+                computerGame.getMinimumGpuMemoryInMegabytes(),
+                computerGame.getDiskSpaceNeededInGB(),
+                computerGame.getRamNeededInGb(),
+                computerGame.getCoreSpeedInGhz(),
+                computerGame.getCoresNeeded()
+            );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AdapterPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            PCGame game = new ComputerGameAdapter(parameters);
+            game.getTitle();
+            game.getPegiAllowedAge();
+            game.isTripleAGame();
+            game.getRequirements();
+            */
+        }
+    }
+}
